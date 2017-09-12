e549f28490eba306d3026b351bc5bcc9ce1e2494
Marten Gajda <marten@dmfs.org>
2017-09-12 20:48:53 +0200

    Remove AbstractMockTable, build SyncedTest with Mockito. Implements #40 (#41)

--- contentpal/src/test/java/org/dmfs/android/contentpal/tables/SyncedTest.java
+++ contentpal/src/test/java/org/dmfs/android/contentpal/tables/SyncedTest.java
@@ -18,22 +18,25 @@ package org.dmfs.android.contentpal.tables;
 
 import android.net.Uri;
 import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
 
 import org.dmfs.android.contentpal.InsertOperation;
 import org.dmfs.android.contentpal.Operation;
 import org.dmfs.android.contentpal.Predicate;
 import org.dmfs.android.contentpal.Table;
 import org.dmfs.android.contentpal.UriParams;
+import org.dmfs.android.contentpal.operations.FailAnswer;
 import org.dmfs.android.contentpal.tools.uriparams.EmptyUriParams;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 
@@ -41,71 +44,51 @@ import static org.mockito.Mockito.mock;
  * @author Marten Gajda
  */
 @RunWith(RobolectricTestRunner.class)
-@Config(manifest = "src/test/AndroidManifest.xml")
+@Config(manifest=Config.NONE)
 public class SyncedTest
 {
     @Test
     public void testInsertOperation() throws Exception
     {
-        final InsertOperation<Object> resultOperation = mock(InsertOperation.class);
-        Table<Object> testTable = new Synced<>(new AbstractMockTable()
-        {
-            @NonNull
-            @Override
-            public InsertOperation<Object> insertOperation(@NonNull UriParams uriParams)
-            {
-                // TODO: how do we check if any other params are still present?
-                assertThat(uriParams.withParam(new Uri.Builder()).build().getBooleanQueryParameter(ContactsContract.CALLER_IS_SYNCADAPTER, false), is(true));
-                return resultOperation;
-            }
-        });
+        final InsertOperation<Object> resultOperation = mock(InsertOperation.class, new FailAnswer());
+        final Table<Object> mockTable = mock(Table.class, new FailAnswer());
+        doReturn(resultOperation).when(mockTable).insertOperation(argThat(new UriParamsArgumentMatcher()));
 
-        assertThat(testTable.insertOperation(new EmptyUriParams()), sameInstance(resultOperation));
+        assertThat(new Synced<>(mockTable).insertOperation(new EmptyUriParams()), sameInstance(resultOperation));
     }
 
 
     @Test
     public void testUpdateOperation() throws Exception
     {
-        final Operation<Object> resultOperation = mock(Operation.class);
+        final Operation<Object> resultOperation = mock(Operation.class, new FailAnswer());
         final Predicate testPredicate = mock(Predicate.class);
-        Table<Object> testTable = new Synced<>(new AbstractMockTable<Object>()
-        {
-            @NonNull
-            @Override
-            public Operation<Object> updateOperation(@NonNull UriParams uriParams, @NonNull Predicate predicate)
-            {
-                assertThat(predicate, sameInstance(testPredicate));
-                // TODO: how do we check if any other params are still present?
-                assertThat(uriParams.withParam(new Uri.Builder()).build().getBooleanQueryParameter(ContactsContract.CALLER_IS_SYNCADAPTER, false), is(true));
-                return resultOperation;
-            }
-        });
+        final Table<Object> mockTable = mock(Table.class, new FailAnswer());
+        doReturn(resultOperation).when(mockTable).updateOperation(argThat(new UriParamsArgumentMatcher()), same(testPredicate));
 
-        assertThat(testTable.updateOperation(new EmptyUriParams(), testPredicate), sameInstance(resultOperation));
+        assertThat(new Synced<>(mockTable).updateOperation(new EmptyUriParams(), testPredicate), sameInstance(resultOperation));
     }
 
 
     @Test
     public void testDeleteOperation() throws Exception
     {
-        final Operation<Object> resultOperation = mock(Operation.class);
+        final Operation<Object> resultOperation = mock(Operation.class, new FailAnswer());
         final Predicate testPredicate = mock(Predicate.class);
-        Table<Object> testTable = new Synced<>(new AbstractMockTable<Object>()
-        {
-            @NonNull
-            @Override
-            public Operation<Object> deleteOperation(@NonNull UriParams uriParams, @NonNull Predicate predicate)
-            {
-                assertThat(predicate, sameInstance(testPredicate));
-                // TODO: how do we check if any other params are still present?
-                assertThat(uriParams.withParam(new Uri.Builder()).build().getBooleanQueryParameter(ContactsContract.CALLER_IS_SYNCADAPTER, false), is(true));
-                return resultOperation;
-            }
-        });
+        final Table<Object> mockTable = mock(Table.class, new FailAnswer());
+        doReturn(resultOperation).when(mockTable).deleteOperation(argThat(new UriParamsArgumentMatcher()), same(testPredicate));
 
-        assertThat(testTable.deleteOperation(new EmptyUriParams(), testPredicate), sameInstance(resultOperation));
+        assertThat(new Synced<>(mockTable).deleteOperation(new EmptyUriParams(), testPredicate), sameInstance(resultOperation));
     }
 
-    // TODO: test view();
+
+    private static class UriParamsArgumentMatcher implements ArgumentMatcher<UriParams>
+    {
+        @Override
+        public boolean matches(UriParams argument)
+        {
+            Uri uri = argument.withParam(new Uri.Builder()).build();
+            return uri.getBooleanQueryParameter(ContactsContract.CALLER_IS_SYNCADAPTER, false);
+        }
+    }
 }
