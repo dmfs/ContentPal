a9419e730a0d2978e49967b8604b5bc957fb781f
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2017-10-05 21:22:12 +0200

    Add missing unit tests for contentpal operations package. #88

---
+++ contentpal/src/test/java/org/dmfs/android/contentpal/operations/BulkAssertTest.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.android.contentpal.operations;
+
+import android.content.ContentProviderOperation;
+import android.net.Uri;
+
+import org.dmfs.android.contentpal.Operation;
+import org.dmfs.android.contentpal.Predicate;
+import org.dmfs.android.contentpal.RowData;
+import org.dmfs.android.contentpal.SoftRowReference;
+import org.dmfs.android.contentpal.Table;
+import org.dmfs.android.contentpal.TransactionContext;
+import org.dmfs.android.contentpal.rowdata.CharSequenceRowData;
+import org.dmfs.android.contentpal.tools.uriparams.EmptyUriParams;
+import org.dmfs.jems.hamcrest.matchers.AbsentMatcher;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.OperationType.assertOperation;
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.WithExpectedCount.withoutExpectedCount;
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.WithValues.withValuesOnly;
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.WithValues.withoutValues;
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.WithYieldAllowed.withYieldNotAllowed;
+import static org.dmfs.android.contentpal.testing.contentvalues.Containing.containing;
+import static org.dmfs.android.contentpal.testing.operations.OperationMatcher.builds;
+import static org.dmfs.android.contentpal.testing.predicates.PredicateSelectionMatcher.predicateWithSelection;
+import static org.dmfs.jems.mockito.doubles.TestDoubles.dummy;
+import static org.dmfs.jems.mockito.doubles.TestDoubles.failingMock;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.doReturn;
+
+
+/**
+ * Unit test for {@link BulkAssert}.
+ *
+ * @author Gabor Keszthelyi
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public final class BulkAssertTest
+{
+
+    @Test
+    public void testReference()
+    {
+        assertThat(new BulkAssert<Object>(dummy(Table.class)).reference(),
+                AbsentMatcher.<SoftRowReference<Object>>isAbsent());
+
+        assertThat(new BulkAssert<Object>(dummy(Table.class), dummy(Predicate.class)).reference(),
+                AbsentMatcher.<SoftRowReference<Object>>isAbsent());
+
+        assertThat(new BulkAssert<Object>(dummy(Table.class), dummy(RowData.class)).reference(),
+                AbsentMatcher.<SoftRowReference<Object>>isAbsent());
+
+        assertThat(new BulkAssert<Object>(dummy(Table.class), dummy(RowData.class), dummy(Predicate.class)).reference(),
+                AbsentMatcher.<SoftRowReference<Object>>isAbsent());
+    }
+
+
+    @Test
+    public void testContentOperationBuilder_ctor_table()
+    {
+        Table<Object> mockTable = failingMock(Table.class);
+        Operation<Object> mockAssertOperation = failingMock(Operation.class);
+
+        doReturn(mockAssertOperation).when(mockTable).assertOperation(same(EmptyUriParams.INSTANCE), predicateWithSelection("1"));
+
+        doReturn(ContentProviderOperation.newAssertQuery(Uri.EMPTY)).when(mockAssertOperation).contentOperationBuilder(any(TransactionContext.class));
+
+        assertThat(new BulkAssert<>(mockTable),
+                builds(
+                        assertOperation(),
+                        withoutExpectedCount(),
+                        withYieldNotAllowed(),
+                        withoutValues()));
+    }
+
+
+    @Test
+    public void testContentOperationBuilder_ctor_table_predicate()
+    {
+        Table<Object> mockTable = failingMock(Table.class);
+        Predicate dummyPredicate = dummy(Predicate.class);
+        Operation<Object> mockAssertOperation = failingMock(Operation.class);
+
+        doReturn(mockAssertOperation).when(mockTable).assertOperation(EmptyUriParams.INSTANCE, dummyPredicate);
+
+        doReturn(ContentProviderOperation.newAssertQuery(Uri.EMPTY)).when(mockAssertOperation).contentOperationBuilder(any(TransactionContext.class));
+
+        assertThat(new BulkAssert<>(mockTable, dummyPredicate),
+                builds(
+                        assertOperation(),
+                        withoutExpectedCount(),
+                        withYieldNotAllowed(),
+                        withoutValues()));
+    }
+
+
+    @Test
+    public void testContentOperationBuilder_ctor_table_data()
+    {
+        Table<Object> mockTable = failingMock(Table.class);
+        Operation<Object> mockAssertOperation = failingMock(Operation.class);
+
+        doReturn(mockAssertOperation).when(mockTable).assertOperation(same(EmptyUriParams.INSTANCE), predicateWithSelection("1"));
+
+        doReturn(ContentProviderOperation.newAssertQuery(Uri.EMPTY)).when(mockAssertOperation).contentOperationBuilder(any(TransactionContext.class));
+
+        assertThat(new BulkAssert<>(mockTable, new CharSequenceRowData<>("key", "value")),
+                builds(
+                        assertOperation(),
+                        withoutExpectedCount(),
+                        withYieldNotAllowed(),
+                        withValuesOnly(
+                                containing("key", "value"))));
+    }
+
+
+    @Test
+    public void testContentOperationBuilder_ctor_table_data_predicate()
+    {
+        Table<Object> mockTable = failingMock(Table.class);
+        Predicate dummyPredicate = dummy(Predicate.class);
+        Operation<Object> mockAssertOperation = failingMock(Operation.class);
+
+        doReturn(mockAssertOperation).when(mockTable).assertOperation(EmptyUriParams.INSTANCE, dummyPredicate);
+
+        doReturn(ContentProviderOperation.newAssertQuery(Uri.EMPTY)).when(mockAssertOperation).contentOperationBuilder(any(TransactionContext.class));
+
+        assertThat(new BulkAssert<>(mockTable, new CharSequenceRowData<>("key", "value"), dummyPredicate),
+                builds(
+                        assertOperation(),
+                        withoutExpectedCount(),
+                        withYieldNotAllowed(),
+                        withValuesOnly(
+                                containing("key", "value"))));
+    }
+
+}
---
+++ contentpal/src/test/java/org/dmfs/android/contentpal/operations/BulkUpdateTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.android.contentpal.operations;
+
+import android.content.ContentProviderOperation;
+import android.net.Uri;
+
+import org.dmfs.android.contentpal.Operation;
+import org.dmfs.android.contentpal.Predicate;
+import org.dmfs.android.contentpal.RowData;
+import org.dmfs.android.contentpal.SoftRowReference;
+import org.dmfs.android.contentpal.Table;
+import org.dmfs.android.contentpal.TransactionContext;
+import org.dmfs.android.contentpal.rowdata.CharSequenceRowData;
+import org.dmfs.android.contentpal.tools.uriparams.EmptyUriParams;
+import org.dmfs.jems.hamcrest.matchers.AbsentMatcher;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.OperationType.updateOperation;
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.WithExpectedCount.withoutExpectedCount;
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.WithValues.withValuesOnly;
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.WithYieldAllowed.withYieldNotAllowed;
+import static org.dmfs.android.contentpal.testing.contentvalues.Containing.containing;
+import static org.dmfs.android.contentpal.testing.operations.OperationMatcher.builds;
+import static org.dmfs.android.contentpal.testing.predicates.PredicateSelectionMatcher.predicateWithSelection;
+import static org.dmfs.jems.mockito.doubles.TestDoubles.dummy;
+import static org.dmfs.jems.mockito.doubles.TestDoubles.failingMock;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.doReturn;
+
+
+/**
+ * Unit test for {@link BulkUpdate}.
+ *
+ * @author Gabor Keszthelyi
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public final class BulkUpdateTest
+{
+
+    @Test
+    public void testReference()
+    {
+        assertThat(new BulkUpdate<Object>(dummy(Table.class), dummy(RowData.class)).reference(),
+                AbsentMatcher.<SoftRowReference<Object>>isAbsent());
+
+        assertThat(new BulkUpdate<Object>(dummy(Table.class), dummy(RowData.class), dummy(Predicate.class)).reference(),
+                AbsentMatcher.<SoftRowReference<Object>>isAbsent());
+    }
+
+
+    @Test
+    public void testContentOperationBuilder_ctor_table_data()
+    {
+        Table<Object> mockTable = failingMock(Table.class);
+        Operation<Object> mockOperation = failingMock(Operation.class);
+
+        doReturn(mockOperation).when(mockTable).updateOperation(same(EmptyUriParams.INSTANCE), predicateWithSelection("1"));
+
+        doReturn(ContentProviderOperation.newUpdate(Uri.EMPTY)).when(mockOperation).contentOperationBuilder(any(TransactionContext.class));
+
+        assertThat(new BulkUpdate<>(mockTable, new CharSequenceRowData<>("key", "value")),
+                builds(
+                        updateOperation(),
+                        withoutExpectedCount(),
+                        withYieldNotAllowed(),
+                        withValuesOnly(
+                                containing("key", "value"))));
+    }
+
+
+    @Test
+    public void testContentOperationBuilder_ctor_table_data_predicate()
+    {
+        Table<Object> mockTable = failingMock(Table.class);
+        Predicate dummyPredicate = dummy(Predicate.class);
+        Operation<Object> mockOperation = failingMock(Operation.class);
+
+        doReturn(mockOperation).when(mockTable).updateOperation(EmptyUriParams.INSTANCE, dummyPredicate);
+
+        doReturn(ContentProviderOperation.newUpdate(Uri.EMPTY)).when(mockOperation).contentOperationBuilder(any(TransactionContext.class));
+
+        assertThat(new BulkUpdate<>(mockTable, new CharSequenceRowData<>("key", "value"), dummyPredicate),
+                builds(
+                        updateOperation(),
+                        withoutExpectedCount(),
+                        withYieldNotAllowed(),
+                        withValuesOnly(
+                                containing("key", "value"))));
+    }
+
+}
