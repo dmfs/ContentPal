a49320df89227c95074410f5617074aaee967f46
Marten Gajda <marten@dmfs.org>
2018-02-08 23:05:33 +0100

    Add `Owned` decorator to set the owner account of a calendar, implements #125. (#157)

---
+++ calendarpal/src/main/java/org/dmfs/android/calendarpal/calendars/Owned.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.android.calendarpal.calendars;
+
+import android.content.ContentProviderOperation;
+import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import org.dmfs.android.contentpal.TransactionContext;
+
+
+/**
+ * {@link CalendarRowData} decorator to set a calendar owner.
+ *
+ * @author Marten Gajda
+ */
+public final class Owned implements CalendarRowData
+{
+    private final CalendarRowData mDelegate;
+    private final CharSequence mOwner;
+
+
+    public Owned(@Nullable CharSequence owner, @NonNull CalendarRowData delegate)
+    {
+        mDelegate = delegate;
+        mOwner = owner;
+    }
+
+
+    @NonNull
+    @Override
+    public ContentProviderOperation.Builder updatedBuilder(TransactionContext transactionContext, @NonNull ContentProviderOperation.Builder builder)
+    {
+        return mDelegate.updatedBuilder(transactionContext, builder)
+                .withValue(CalendarContract.Calendars.OWNER_ACCOUNT, mOwner == null ? null : mOwner.toString());
+    }
+}
