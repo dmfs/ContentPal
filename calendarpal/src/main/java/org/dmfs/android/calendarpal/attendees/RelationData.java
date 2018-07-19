0c9cf6176189710ecde62277b11e78030f1f5653
Marten Gajda <marten@dmfs.org>
2018-07-20 00:33:56 +0200

    Split AttendeeData into smaller classes, implements #166 (#167)
    This commit splits `AttendeeData` into smaller, more SRP-ish classes. Now there is a class for each attendee value.
    
    Note, `AttendeeData` still sets the required defaults of the attendde relationship, status and type. They can be overridden by supplying an attributes `RowData<Attendees>` object.

---
+++ calendarpal/src/main/java/org/dmfs/android/calendarpal/attendees/RelationData.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.android.calendarpal.attendees;
+
+import android.content.ContentProviderOperation;
+import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+
+import org.dmfs.android.contentpal.RowData;
+import org.dmfs.android.contentpal.TransactionContext;
+
+
+/**
+ * {@link CalendarContract.Attendees} {@link RowData} which sets the {@link CalendarContract.Attendees#ATTENDEE_RELATIONSHIP} of an attendee.
+ *
+ * @author Marten Gajda
+ */
+public final class RelationData implements RowData<CalendarContract.Attendees>
+{
+    private final int mRelationship;
+
+
+    public RelationData(int relationship)
+    {
+        mRelationship = relationship;
+    }
+
+
+    @NonNull
+    @Override
+    public ContentProviderOperation.Builder updatedBuilder(@NonNull TransactionContext transactionContext, @NonNull ContentProviderOperation.Builder builder)
+    {
+        return builder.withValue(CalendarContract.Attendees.ATTENDEE_RELATIONSHIP, mRelationship);
+    }
+}
---
+++ calendarpal/src/test/java/org/dmfs/android/calendarpal/attendees/TypeDataTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.android.calendarpal.attendees;
+
+import android.provider.CalendarContract;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.WithValues.withValuesOnly;
+import static org.dmfs.android.contentpal.testing.contentvalues.Containing.containing;
+import static org.dmfs.android.contentpal.testing.rowdata.RowDataMatcher.builds;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Test for {@link TypeData}.
+ *
+ * @author Marten Gajda
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class TypeDataTest
+{
+    @Test
+    public void test() throws Exception
+    {
+        assertThat(new TypeData(1234), builds(
+                withValuesOnly(
+                        containing(CalendarContract.Attendees.ATTENDEE_TYPE, 1234)
+                )));
+    }
+
+}
