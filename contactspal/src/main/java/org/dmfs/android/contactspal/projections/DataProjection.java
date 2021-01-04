1166f0c9fed4a04764da19f9850031a3989c0abd
Marten Gajda <marten@dmfs.org>
2021-01-05 00:42:03 +0100

    Remove is_read_only column from DataProjection. Implements #221 (#222)
    The columns doesn't seem to be readable on all device. Since we barely
    need it we drop it for now.

--- contactspal/src/main/java/org/dmfs/android/contactspal/projections/DataProjection.java
+++ contactspal/src/main/java/org/dmfs/android/contactspal/projections/DataProjection.java
@@ -27,8 +27,6 @@ import org.dmfs.android.contentpal.projections.MultiProjection;
  * A {@link Projection} of all {@link ContactsContract.Data} columns which are present since API level 11.
  * <p>
  * Note, this doesn't include any joined columns from {@link ContactsContract.RawContacts}.
- *
- * @author Marten Gajda
  */
 public final class DataProjection extends DelegatingProjection<ContactsContract.Data>
 {
@@ -36,31 +34,30 @@ public final class DataProjection extends DelegatingProjection<ContactsContract.
     {
         // TODO: it's probably better to compose this from smaller projections with just a few columns
         super(new MultiProjection<>(
-                ContactsContract.Data._ID,
-                ContactsContract.Data.MIMETYPE,
-                ContactsContract.Data.RAW_CONTACT_ID,
-                ContactsContract.Data.IS_PRIMARY,
-                ContactsContract.Data.IS_SUPER_PRIMARY,
-                ContactsContract.Data.DATA_VERSION,
-                ContactsContract.Data.IS_READ_ONLY,
-                ContactsContract.Data.DATA1,
-                ContactsContract.Data.DATA2,
-                ContactsContract.Data.DATA3,
-                ContactsContract.Data.DATA4,
-                ContactsContract.Data.DATA5,
-                ContactsContract.Data.DATA6,
-                ContactsContract.Data.DATA7,
-                ContactsContract.Data.DATA8,
-                ContactsContract.Data.DATA9,
-                ContactsContract.Data.DATA10,
-                ContactsContract.Data.DATA11,
-                ContactsContract.Data.DATA12,
-                ContactsContract.Data.DATA13,
-                ContactsContract.Data.DATA14,
-                ContactsContract.Data.DATA15,
-                ContactsContract.Data.SYNC1,
-                ContactsContract.Data.SYNC2,
-                ContactsContract.Data.SYNC3,
-                ContactsContract.Data.SYNC4));
+            ContactsContract.Data._ID,
+            ContactsContract.Data.MIMETYPE,
+            ContactsContract.Data.RAW_CONTACT_ID,
+            ContactsContract.Data.IS_PRIMARY,
+            ContactsContract.Data.IS_SUPER_PRIMARY,
+            ContactsContract.Data.DATA_VERSION,
+            ContactsContract.Data.DATA1,
+            ContactsContract.Data.DATA2,
+            ContactsContract.Data.DATA3,
+            ContactsContract.Data.DATA4,
+            ContactsContract.Data.DATA5,
+            ContactsContract.Data.DATA6,
+            ContactsContract.Data.DATA7,
+            ContactsContract.Data.DATA8,
+            ContactsContract.Data.DATA9,
+            ContactsContract.Data.DATA10,
+            ContactsContract.Data.DATA11,
+            ContactsContract.Data.DATA12,
+            ContactsContract.Data.DATA13,
+            ContactsContract.Data.DATA14,
+            ContactsContract.Data.DATA15,
+            ContactsContract.Data.SYNC1,
+            ContactsContract.Data.SYNC2,
+            ContactsContract.Data.SYNC3,
+            ContactsContract.Data.SYNC4));
     }
 }
