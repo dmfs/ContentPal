73d83536ef4ce710d5bad8d6ff2b495b149ec66c
Marten Gajda <marten@dmfs.org>
2021-01-05 00:48:03 +0100

    Add Empty Predicate, implements #225 (#226)

---
+++ contentpal/src/main/java/org/dmfs/android/contentpal/predicates/Empty.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.android.contentpal.predicates;
+
+import org.dmfs.android.contentpal.Predicate;
+
+import androidx.annotation.NonNull;
+
+
+/**
+ * A {@link Predicate} which matches any row containing a {@code null} value or an empty String.
+ */
+public final class Empty<Contract> extends DelegatingPredicate<Contract>
+{
+    public Empty(@NonNull String column)
+    {
+        super(
+            new AnyOf<>(
+                new IsNull<>(column),
+                new EqArg<>(column, "")));
+    }
+}
