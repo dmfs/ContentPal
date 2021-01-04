86d5b9a9c5ef72e33a713a5edf6bf23ad5cc126d
Marten Gajda <marten@dmfs.org>
2021-01-05 00:47:41 +0100

    Add IdProjection, implements #223 (#224)
    Add a Projection which contains only the BaseColumns._ID column.

---
+++ contentpal/src/main/java/org/dmfs/android/contentpal/projections/IdProjection.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.android.contentpal.projections;
+
+import android.provider.BaseColumns;
+
+import org.dmfs.android.contentpal.Projection;
+
+import androidx.annotation.NonNull;
+
+
+/**
+ * A {@link Projection} which contains only the {@link BaseColumns#_ID} column.
+ */
+public final class IdProjection<T> implements Projection<T>
+{
+    @NonNull
+    @Override
+    public String[] toArray()
+    {
+        return new String[] { BaseColumns._ID };
+    }
+}
