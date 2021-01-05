d3d7e2d5e9fe5fde998b178cf2bb4599c10f0d52
Marten Gajda <marten@dmfs.org>
2021-01-05 01:14:26 +0100

    Add Groups Tables and Views. Implements #9, #8 (#220)

---
+++ contactspal/src/main/java/org/dmfs/android/contactspal/views/Groups.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.android.contactspal.views;
+
+import android.content.ContentProviderClient;
+import android.provider.ContactsContract;
+
+import org.dmfs.android.contentpal.View;
+import org.dmfs.android.contentpal.views.BaseView;
+import org.dmfs.android.contentpal.views.DelegatingView;
+
+import androidx.annotation.NonNull;
+
+
+/**
+ * The ContactsProvider {@link ContactsContract.Groups} {@link View}.
+ */
+public final class Groups extends DelegatingView<ContactsContract.Groups>
+{
+    public Groups(@NonNull ContentProviderClient client)
+    {
+        super(new BaseView<>(client, ContactsContract.Groups.CONTENT_URI));
+    }
+}
